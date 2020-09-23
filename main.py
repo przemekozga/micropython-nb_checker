5f741dbcd2c1eb0246a50292f505eee7ff6d41a0
przemekozga <70341418+przemekozga@users.noreply.github.com>
2020-09-23 10:40:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from system_devices import *
 checker = system_devices(config="config.json")
 xbee = checker.device('xbee')
 gps = checker.device('gps')
+mpu = checker.device('mpu')
 pwm = checker.pwm(wait_seconds=5)
